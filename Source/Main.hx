42eba059aa21dc16260d2b59092c7663580b0c43
matrefeytontias <mattias@refeyton.fr>
2017-08-09 20:53:27 +0200

    Added mockup code of cube demo

--- Source/Main.hx
+++ Source/Main.hx
@@ -1,17 +1,34 @@
 import fourDee.Application;
 import fourDee.math.*;
 
+import lime.ui.Window;
+
 class Main extends Application
 {
+	private var hcube:Object4D;
+	
 	public function new()
 	{
 		super();
+	}
+	
+	override public function onWindowCreate(window:Window)
+	{
+		super.onWindowCreate(window);
+		space4D = new Space4D(window.width, window.height);
 		
-		var v = new Vector4();
-		var e = new Euler4(Math.PI / 2);
-		e.center.x = -1.;
-		var m = e.makeMatrix();
-		trace(m);
-		trace(m * v);
+		// Screen width, screen width, fov in degrees
+		space4D.attachCamera(new PerspectiveCamera(90));
+		
+		hcube = new Mesh4D(new BoxGeometry4D(1, 1, 1, 1), new SolidMaterial(0xff0000));
+		space4D.add(hcube);
+		hcube.position.z = -5;
+	}
+	
+	override public function update(dt:Int)
+	{
+		hcube.rotation.xz += dt;
+		hcube.rotation.yz -= dt / 2;
+		super.update(dt);
 	}
 }
