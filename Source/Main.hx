0302c767a276d502e053b7fceb52021cff854b72
matrefeytontias <mattias@refeyton.fr>
2017-08-09 07:11:49 +0200

    Math test

--- Source/Main.hx
+++ Source/Main.hx
@@ -1,9 +1,17 @@
 import fourDee.Application;
+import fourDee.math.*;
+import fourDee.math.Matrix5.Rotation4D;
 
 class Main extends Application
 {
 	public function new()
 	{
 		super();
+		
+		var v = new Vector4(1, 2, 3, 4);
+		var m = new Matrix5();
+		m.makeRotation(Rotation4D.XY, Math.PI / 2);
+		var v2 = m * v;
+		trace(v, v2);
 	}
 }
