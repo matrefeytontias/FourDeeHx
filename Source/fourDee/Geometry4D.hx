6fdc6911ec727e0dd55c40f656edffc439424fac
matrefeytontias <mattias@refeyton.fr>
2017-08-13 00:05:36 +0545

    Added 4D geometry extrusion from 3D ; added 4D box geometry

--- Source/fourDee/Geometry4D.hx
+++ Source/fourDee/Geometry4D.hx
@@ -1,16 +1,52 @@
 package fourDee;
 
 import fourDee.math.Vector4;
-import fourDee.render.Face4;
+import fourDee.render.Face3;
+import fourDee.render.Cell4;
+
+typedef Vector3 = lime.math.Vector4;
 
 class Geometry4D
 {
 	public var vertices:Array<Vector4>;
-	public var faces:Array<Face4>;
+	public var cells:Array<Cell4>;
 	
-	private function new(?v:Array<Vector4>, ?f:Array<Face4>)
+	private function new(?v:Array<Vector4>, ?c:Array<Cell4>)
 	{
 		vertices = v == null ? new Array<Vector4>() : v;
-		faces = f == null ? new Array<Face4>() : f;
+		cells = c == null ? new Array<Cell4>() : c;
+	}
+	
+	public function from3D(vertices3D:Array<Vector3>, faces3D:Array<Face3>, duth:Float)
+	{
+		var extraVertexOffset = vertices3D.length;
+		var d = duth / 2;
+		var v4 = new Vector4();
+		for(i in 0 ... vertices3D.length)
+		{
+			// Copy the "normal" vectors
+			var v = vertices3D[i];
+			v4.setTo(v.x, v.y, v.z, -d);
+			vertices.push(v4.clone());
+		}
+		// Extrude all vertices along w
+		for(i in 0 ... vertices3D.length)
+		{
+			v4.copyFrom(vertices[i]);
+			v4.w = d;
+			vertices.push(v4.clone());
+		}
+		// Make a 4D prism by extruding triangles along w
+		// A prism is made of 3 tetrahedra ; those are the 4D model's cells
+		for(f3 in 0 ... faces3D.length)
+		{
+			var face = faces3D[f3];
+			var a = face.a, b = face.b, c = face.c,
+			d = face.a + extraVertexOffset, e = face.b + extraVertexOffset, f = face.c + extraVertexOffset;
+			// Draw it if you don't believe me
+			cells.push(new Cell4(a, c, e, d));
+			cells.push(new Cell4(b, e, c, a));
+			cells.push(new Cell4(c, e, f, d));
+		}
 	}
 }
--- Source/fourDee/geometries/BoxGeometry4D.hx
+++ Source/fourDee/geometries/BoxGeometry4D.hx
@@ -1,9 +1,32 @@
 package fourDee.geometries;
 
+import fourDee.math.Vector3;
+import fourDee.render.Face3;
+
 class BoxGeometry4D extends Geometry4D
 {
+	static private var cubeFaces = [ new Face3(0, 1, 2), new Face3(0, 2, 3),
+		new Face3(4, 6, 5), new Face3(4, 7, 6),
+		new Face3(0, 3, 7), new Face3(0, 7, 4),
+		new Face3(1, 6, 2), new Face3(1, 5, 6),
+		new Face3(2, 6, 7), new Face3(2, 7, 3),
+		new Face3(1, 0, 4), new Face3(1, 4, 5)
+	];
+	
 	public function new(w:Float, h:Float, d:Float, du:Float)
 	{
 		super();
+		
+		var vertices3 = [ new Vector3(-w / 2, -h / 2, d / 2),
+			new Vector3(w / 2, -h / 2, d / 2),
+			new Vector3(w / 2, h / 2, d / 2),
+			new Vector3(-w / 2, h / 2, d / 2),
+			new Vector3(-w / 2, -h / 2, -d / 2),
+			new Vector3(w / 2, -h / 2, -d / 2),
+			new Vector3(w / 2, h / 2, -d / 2),
+			new Vector3(-w / 2, h / 2, -d / 2)
+		];
+		
+		from3D(vertices3, cubeFaces, du);
 	}
 }
