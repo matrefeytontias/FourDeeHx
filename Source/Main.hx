f4733a632712ecfbdaf4ce8827815e019f794d13
matrefeytontias <mattias@refeyton.fr>
2017-08-13 00:04:04 +0545

    Changed mockup to match planned syntax

--- Source/Main.hx
+++ Source/Main.hx
@@ -1,15 +1,15 @@
 import fourDee.Application;
 import fourDee.Space4D;
 import fourDee.geometries.BoxGeometry4D;
-import fourDee.materials.SolidMaterial;
-import fourDee.objects.Box;
+import fourDee.materials.*;
+import fourDee.objects.Mesh4D;
 import fourDee.render.PerspectiveCamera;
 
 import lime.ui.Window;
 
 class Main extends Application
 {
-	private var hcube:Box;
+	private var hcube:Mesh4D;
 	
 	public function new()
 	{
@@ -24,17 +24,18 @@ class Main extends Application
 		// Screen width, screen height, fov in degrees
 		space4D.attachCamera(new PerspectiveCamera(90, window.width / window.height));
 		
-		hcube = new Box(1, 1, 1, new SolidMaterial(0xff0000));
+		hcube = new Mesh4D(new BoxGeometry4D(1, 1, 1, 1), new SolidMaterial(0xff0000));
 		space4D.add(hcube);
 		hcube.position.z = -5;
+		
+		var GL = lime.graphics.opengl.GL;
+		// GL.enable(GL.CULL_FACE);
 	}
 	
 	override public function update(dt:Int)
 	{
-		// hcube.rotation.xz += dt;
-		// hcube.rotation.yz -= dt / 2;
-		space4D.camera.rotation3D.x -= dt / 1000;
-		space4D.camera.rotation3D.y += dt / 1000;
+		hcube.rotation.xw -= dt / 1000;
+		hcube.rotation.xz += dt / 1000;
 		super.update(dt);
 	}
 }
