74586b5687f1918a8ec6a1af19effc18f885d7a1
matrefeytontias <mattias@refeyton.fr>
2017-08-13 00:09:03 +0545

    Proper dispatch to materials of render commands

--- Source/fourDee/Material.hx
+++ Source/fourDee/Material.hx
@@ -1,25 +1,46 @@
 package fourDee;
 
-import fourDee.Object4D;
 import fourDee.render.Camera;
+import fourDee.render.ObjectSlice3D;
 
+import lime.graphics.opengl.GL;
 import lime.graphics.opengl.GLBuffer;
 import lime.graphics.opengl.GLProgram;
+import lime.graphics.opengl.GLUniformLocation;
+import lime.math.Matrix4;
+import lime.math.Vector4;
+import lime.utils.GLUtils;
 
 class Material
 {
 	private var program:GLProgram;
+	private var glAPosition:Int;
+	private var glANormal:Int;
+	private var glUMat:GLUniformLocation;
 	
-	private function new()
+	private function new(vert:String, frag:String)
 	{
+		program = GLUtils.createProgram(vert, frag);
+		glAPosition = GL.getAttribLocation(program, "aPosition");
+		glANormal = GL.getAttribLocation(program, "aNormal");
+		GL.enableVertexAttribArray(glAPosition);
+		GL.enableVertexAttribArray(glANormal);
+		glUMat = GL.getUniformLocation(program, "mat");
 	}
 	
-	public function setupRender(gl:lime.graphics.GLRenderContext, obj:Object4D, glBuffer:GLBuffer, camera:Camera)
+	public function setupRender(gl:lime.graphics.GLRenderContext, obj:ObjectSlice3D, glBuffer:GLBuffer, camera:Camera)
 	{
-		trace("Woops");
+		gl.useProgram(program);
+		gl.bindBuffer(gl.ARRAY_BUFFER, glBuffer);
+		gl.vertexAttribPointer(glAPosition, 3, gl.FLOAT, false, 6 * 4, 0);
+		gl.vertexAttribPointer(glANormal, 3, gl.FLOAT, false, 6 * 4, 3 * 4);
+		var mat = camera.matrix3D.clone();
+		mat.append(camera.pmat);
+		gl.uniformMatrix4fv(glUMat, 1, false, mat);
 	}
 	
 	public function cleanupRender(gl:lime.graphics.GLRenderContext)
 	{
+		gl.bindBuffer(gl.ARRAY_BUFFER, null);
 	}
 }
--- Source/fourDee/materials/LambertMaterial.hx
+++ Source/fourDee/materials/LambertMaterial.hx
@@ -10,46 +10,62 @@ import lime.graphics.opengl.GLBuffer;
 import lime.graphics.opengl.GLUniformLocation;
 import lime.math.Matrix4;
 
-class SolidMaterial extends Material
+class LambertMaterial extends Material
 {
-	inline static private var solidVert =
+	inline static private var lambertVert =
 		"
 		attribute vec3 aPosition;
+		attribute vec3 aNormal;
+		
 		uniform mat4 mat;
-		uniform mat4 rmat;
+		
+		varying vec3 position;
+		varying vec3 normal;
 		
 		void main()
 		{
+			normal = aNormal;
+			position = aPosition;
 			gl_Position = mat * vec4(aPosition, 1.);
 		}
 		";
-	inline static private var solidFrag =
+	inline static private var lambertFrag =
 #if !desktop
 		"precision mediump float;" +
 #end
 		"
 			uniform vec4 color;
+			uniform float diffuse;
+			
+			varying vec3 position;
+			varying vec3 normal;
 			
 			void main()
 			{
-				gl_FragColor = color;
+				float shade = max(0., dot(-normalize(position), normal)) * diffuse;
+				gl_FragColor = color * shade;
 			}
 		";
 	
 	public var color:Int;
+	public var diffuse:Float;
 	
 	private var glUColor:GLUniformLocation;
+	private var glUDiffuse:GLUniformLocation;
 	
-	public function new(c:Int)
+	public function new(c:Int, d:Float)
 	{
-		super(solidVert, solidFrag);
+		super(lambertVert, lambertFrag);
 		color = c;
+		diffuse = d;
 		glUColor = GL.getUniformLocation(program, "color");
+		glUDiffuse = GL.getUniformLocation(program, "diffuse");
 	}
 	
 	override public function setupRender(gl:lime.graphics.GLRenderContext, obj:ObjectSlice3D, glBuffer:GLBuffer, camera:Camera)
 	{
 		super.setupRender(gl, obj, glBuffer, camera);
 		gl.uniform4f(glUColor, (color >> 16) / 255, ((color >> 8) & 0xff) / 255, (color & 0xff) / 255, 1.);
+		gl.uniform1f(glUDiffuse, diffuse);
 	}
 }
